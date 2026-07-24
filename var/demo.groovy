e099d4b59e6389e915628196f6f15b7315b7ff24
avinash20236 <avinashavi5143@gmail.com>
2026-07-24 12:02:00 +0530

    Refactor pipeline definition to use a callable function

---
+++ var/demo.groovy
@@ -0,0 +1,61 @@
+def call(){
+  pipeline {
+    agent {
+        label "Vinod"
+    }
+
+    stages {
+
+        stage("code") {
+            steps {
+                echo "this is for coding"
+                git url: "https://github.com/avinash20236/main-project-1", branch: "main"
+                echo "cloning is successful"
+            }
+        }
+
+        stage("Check User") {
+            steps {
+                sh 'whoami'
+                sh 'id'
+                sh 'groups'
+                sh 'docker ps'
+            }
+        }
+
+        stage("build") {
+            steps {
+                echo "this is for building code"
+                sh 'docker build -t notes-app:latest .'
+            }
+        }
+
+        stage("Push to Dockerhub") {
+            steps {
+                echo "this is pushing image to dockerhub"
+
+                withCredentials([usernamePassword(
+                    credentialsId: 'dockerhubcred',
+                    usernameVariable: 'dockerhubuser',
+                    passwordVariable: 'dockerhubpass'
+                )]) {
+
+                    sh '''
+                        echo "$dockerhubpass" | docker login -u "$dockerhubuser" --password-stdin
+                        docker image tag notes-app:latest $dockerhubuser/notes-app:latest
+                        docker push $dockerhubuser/notes-app:latest
+                    '''
+                }
+            }
+        }
+
+        stage("Deploy") {
+            steps {
+                echo "this is for Deploying code"
+                sh 'docker-compose up -d'
+            }
+        }
+    }
+}
+
+}
